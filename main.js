b0bffed8480346421949a689cd9b02052f8f6bb6
Talaat <talaatgamal.dev@gmail.com>
2024-09-18 00:41:15 +0300

    first commit

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 }
+
+
+// إغلاق القائمة عند الضغط على أي رابط
+var links = document.querySelectorAll('#nav-links a');
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        var navLinks = document.getElementById("nav-links");
+        navLinks.classList.remove("active"); // إغلاق القائمة
+    });
+});
